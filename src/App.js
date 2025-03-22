ef420c380534ec819ce719a983e54e0cd083d31a
sachinravi285. <ravisachin797@gmail.com>
2025-03-22 08:57:03 +0530

    Update repository

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
